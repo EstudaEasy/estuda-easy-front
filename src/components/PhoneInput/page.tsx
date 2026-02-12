b078a1998b898bdd58400a2b5505086650df07b3
Rafael-acm1 <117954682+Rafael-acm1@users.noreply.github.com>
2026-02-12 18:07:40 -0300

    feat: create phone input component with Brazilian format mask

---
+++ src/components/PhoneInput/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useIMask } from 'react-imask';
+import styles from '@/components/FormInput/styles.module.css';
+
+interface PhoneInputProps {
+  value: string;
+  onChange: (value: string) => void;
+  required?: boolean;
+}
+
+export function PhoneInput({ value, onChange, required = false }: PhoneInputProps) {
+  const { ref } = useIMask(
+    {
+      mask: '+55 (00) 00000-0000',
+    },
+    {
+      onAccept: (value) => {
+        onChange(value as string);
+      },
+    }
+  );
+
+  return (
+    <input
+      ref={ref}
+      type="tel"
+      placeholder="+55 (11) 98765-4321"
+      className={styles.input}
+      required={required}
+      defaultValue={value}
+    />
+  );
+}
