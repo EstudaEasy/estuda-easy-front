1d74304f3873f3c8515c4f7a5ccd99873752b41f
Rafael-acm1 <117954682+Rafael-acm1@users.noreply.github.com>
2026-03-10 18:23:31 -0300

    feat: add whiteboard form component with validation

---
+++ src/components/ViewWhiteboard/WhiteboardForm/whiteboardForm.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { quizFormSchema } from "../../QuizForm/quizForm.schema";
+
+export const whiteboardFormSchema = z.object({
+  title: z
+    .string()
+    .min(3, "O título deve ter no mínimo 3 caracteres")
+    .max(100, "O título deve ter no máximo 100 caracteres"),
+});
+
+export type WhiteboardFormData = z.infer<typeof whiteboardFormSchema>;
